e78a2ec60b453e3d5aaf45b2ef89f42f593ef56f
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2021-07-19 10:47:51 +0530

    This commit contains Soundpool Build Changes

--- app/src/main/java/com/rl/xylophoneapp/MainActivity.java
+++ app/src/main/java/com/rl/xylophoneapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.rl.xylophoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
@@ -35,46 +36,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC,0);
+        //mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC,0);
 
-        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
-        mDSoundId = mSoundPool.load(getApplicationContext(),R.raw.note2_d,1);
-        mESoundId = mSoundPool.load(getApplicationContext(),R.raw.note3_e,1);
-        mFSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f,1);
-        mGSoundId = mSoundPool.load(getApplicationContext(),R.raw.note5_g,1);
-        mASoundId = mSoundPool.load(getApplicationContext(),R.raw.note6_a,1);
-        mBSoundId = mSoundPool.load(getApplicationContext(),R.raw.note7_b,1);
+        mSoundPool = new SoundPool.Builder().
+                setMaxStreams(NR_OF_SIMULTANEOUS_SOUNDS)
+                .build();
+
+
+        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
 
 
     }
 
     public void playC(View view) {
 
-        mSoundPool.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
     public void playD(View view) {
-        mSoundPool.play(mDSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
     public void playE(View view) {
-        mSoundPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
     public void playF(View view) {
-        mSoundPool.play(mFSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
     public void playG(View view) {
-        mSoundPool.play(mGSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
     public void playA(View view) {
-        mSoundPool.play(mASoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mASoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
     public void playB(View view) {
-        mSoundPool.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOOP,NORMAL_PLAY_RATE);
+        mSoundPool.play(mBSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOOP, NORMAL_PLAY_RATE);
     }
 
 
